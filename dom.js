7bfdba8a7ae4afcdd4aad0b248a2c53e5b0f23b1
Mayanktyagi05 <mayanktyagee@gmail.com>
2023-08-10 22:34:25 +0530

    commit by tagname

--- dom.js
+++ dom.js
@@ -1,6 +1,2 @@
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = "green";
-for(var i=0; i<items.length; i++)
-{
-    items[i].style.fontWeight ="bold";
-}
+var li = document.getElementsByTagName('li');
+li[4].style.backgroundColor = "yellow";
